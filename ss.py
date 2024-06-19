563f07ef5ab6ba155637fc79b906ce52f27be3e3
Lowell Javier <136763472+Lviers@users.noreply.github.com>
2024-06-18 21:16:57 -0700

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,42 @@
+"""apple = "Hello World!"
+
+print(apple[-1])
+
+var = input("Input Word: ")
+index = int("Input Number: ")
+print(var[index])
+print(len[var])
+"""
+
+#slicing 
+"""
+butter = "Hello World"
+
+print(butter[-5:11]) #World
+print(butter[-8:-7], butter[6]) #l W
+print("{0} {1}".format(butter[6:8], butter[0:2])) #Wo He
+
+print(f"{butter[0:2]} {butter[6:8]}") #He Wo
+"""
+
+#stwing mwethods OWO
+"""
+wowd = "hewoo owo"
+print(wowd.upper()) #HEWOO OWO
+print(wowd.lower()) #hewoo owo
+print(wowd.capitalize()) #Hewoo owo
+print(wowd.title()) # Hewoo Owo
+print(wowd.replace("h", "nigga")) # niggaewoo owo
+print(wowd.find("h")) #nuwmber 
+print(wowd.isalpha()) #spwace is nwot an awpha UWU
+print(wowd.isnumeric())
+print(wowd.isalnum()) #spwace is nwot an awphanum OMO
+"""
+
+word = "summer bootcamp"
+print(word.title()) 
+print(word.capitalize().replace("p","P")) 
+print(word[7:11].replace("b", "L")) 
+print(word[11:].replace("p", "E")) 
+print(word[12].upper() + word[5].upper()) 
+print(word.replace(" ", "ss").isalpha())
